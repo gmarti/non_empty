b256db30e63c9962f7fb3540802c94c62eb84ddb
Grégory marti <greg.marti@gmail.com>
2024-01-10 15:59:24 +0100

    Initial commit

---
+++ src/slice.rs
@@ -0,0 +1,206 @@
+use std::{fmt, ops::Deref};
+
+use super::NonEmptyVec;
+
+#[derive(PartialEq, Eq)]
+#[repr(transparent)]
+pub struct NonEmptySlice<T> {
+    inner: [T],
+}
+
+mod error {
+    #[derive(Debug)]
+    pub struct Empty;
+}
+
+impl<T> NonEmptySlice<T> {
+    pub fn try_from_slice(slice: &[T]) -> Result<&NonEmptySlice<T>, error::Empty> {
+        if !slice.is_empty() {
+            Ok(unsafe { NonEmptySlice::new_unchecked(slice) })
+        } else {
+            Err(error::Empty)
+        }
+    }
+
+    pub(super) const unsafe fn new_unchecked(slice: &[T]) -> &NonEmptySlice<T> {
+        debug_assert!(!slice.is_empty());
+        // SAFETY: This type is `repr(transparent)`, so we can safely
+        // cast the references like this.
+        &*(slice as *const [T] as *const NonEmptySlice<T>)
+    }
+
+    pub(super) unsafe fn unchecked_boxed(slice: Box<[T]>) -> Box<Self> {
+        debug_assert!(!slice.is_empty());
+        // SAFETY: This type is `repr(transparent)`, so we can safely
+        // cast the pointers like this.
+        // `Box` does not necessarily have a guaranteed type layout
+        // so it's safer to use methods to convert to/from raw pointers.
+        let ptr = Box::into_raw(slice) as *mut Self;
+        Box::from_raw(ptr)
+    }
+
+    pub fn first(&self) -> &T {
+        &self.inner[0]
+    }
+
+    pub fn tail(&self) -> &[T] {
+        &self.inner[1..]
+    }
+
+    pub fn last(&self) -> &T {
+        &self.inner[self.len() - 1]
+    }
+
+    pub fn init(&self) -> &[T] {
+        &self.inner[..self.len() - 1]
+    }
+
+    pub fn split_first(&self) -> (&T, &[T]) {
+        (self.first(), self.tail())
+    }
+
+    pub fn split_last(&self) -> (&[T], &T) {
+        (self.init(), self.last())
+    }
+
+    pub fn as_slice(&self) -> &[T] {
+        &self.inner
+    }
+}
+
+impl<T: Clone> NonEmptySlice<T> {
+    pub fn to_vec(&self) -> NonEmptyVec<T> {
+        self.inner.to_vec().try_into().unwrap()
+    }
+}
+
+impl<T: Clone> Clone for Box<NonEmptySlice<T>> {
+    fn clone(&self) -> Self {
+        self.to_vec().into_boxed_slice()
+    }
+}
+
+impl<T: fmt::Debug> fmt::Debug for NonEmptySlice<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(&self.inner, f)
+    }
+}
+
+impl<'a, T> IntoIterator for &'a NonEmptySlice<T> {
+    type Item = &'a T;
+    type IntoIter = std::slice::Iter<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+impl<T> Deref for NonEmptySlice<T> {
+    type Target = [T];
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl<'a, T> TryFrom<&'a [T]> for &'a NonEmptySlice<T> {
+    type Error = error::Empty;
+
+    #[inline]
+    fn try_from(value: &'a [T]) -> Result<Self, Self::Error> {
+        NonEmptySlice::try_from_slice(value)
+    }
+}
+
+impl<T> TryFrom<Box<[T]>> for Box<NonEmptySlice<T>> {
+    type Error = error::Empty;
+
+    fn try_from(value: Box<[T]>) -> Result<Self, Self::Error> {
+        if !value.is_empty() {
+            // SAFETY: We just checked that it's not empty,
+            // so we can safely create a `NonEmptySlice`.
+            Ok(unsafe { NonEmptySlice::unchecked_boxed(value) })
+        } else {
+            Err(error::Empty)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::non_empty_vec;
+
+    #[test]
+    fn from_non_empty_vec() {
+        let non_empty_slice: &NonEmptySlice<i32> = &non_empty_vec![10, 20, 30, 40, 50];
+
+        assert_eq!(non_empty_slice.as_slice(), &[10, 20, 30, 40, 50])
+    }
+
+    #[test]
+    fn debug() {
+        let multiple: &NonEmptySlice<i32> = &non_empty_vec![10, 20, 30, 40, 50];
+
+        let result = format!("{multiple:?}");
+        assert_eq!(result, "[10, 20, 30, 40, 50]");
+    }
+
+    #[test]
+    fn split() {
+        let multiple: &NonEmptySlice<i32> = &non_empty_vec![10, 20, 30, 40, 50];
+
+        assert_eq!(multiple.split_first(), (&10, &[20, 30, 40, 50][..]));
+        assert_eq!(multiple.split_last(), (&[10, 20, 30, 40][..], &50));
+    }
+
+    #[test]
+    fn non_empty_slice_of_simple_struct() {
+        // No clone, no PartialEq, no Eq
+        struct Test(usize);
+
+        let non_empty_slice: &NonEmptySlice<Test> = &non_empty_vec![Test(0)];
+
+        assert!(non_empty_slice.first().0 == 0);
+    }
+
+    #[test]
+    fn new() -> Result<(), error::Empty> {
+        let vec = vec![10, 20, 30];
+        let result = NonEmptySlice::try_from_slice(&vec)?;
+
+        assert_eq!(result.len(), 3);
+        assert_eq!(result[0], 10);
+        assert_eq!(result[1], 20);
+        assert_eq!(result.first(), &10);
+        assert_eq!(result.last(), &30);
+
+        let vec: Vec<i32> = Vec::new();
+        let result = NonEmptySlice::try_from_slice(&vec);
+
+        assert!(result.is_err());
+
+        Ok(())
+    }
+
+    #[test]
+    fn try_from_slice() -> Result<(), error::Empty> {
+        let vec = [10, 20, 30];
+        let result: &NonEmptySlice<i32> = vec[..].try_into()?;
+
+        assert_eq!(result.len(), 3);
+        assert_eq!(result[0], 10);
+        assert_eq!(result[1], 20);
+        assert_eq!(result.first(), &10);
+        assert_eq!(result.last(), &30);
+
+        let vec: Vec<i32> = Vec::new();
+        let result: Result<&NonEmptySlice<i32>, _> = vec[..].try_into();
+
+        assert!(result.is_err());
+
+        Ok(())
+    }
+}
---
+++ src/vec.rs
@@ -0,0 +1,278 @@
+use std::{fmt, ops::Deref};
+
+use super::slice::NonEmptySlice;
+
+#[derive(Clone, PartialEq, Eq)]
+pub struct NonEmptyVec<T> {
+    inner: Vec<T>,
+}
+
+mod error {
+    #[derive(Debug)]
+    pub struct Empty;
+}
+
+impl<T> NonEmptyVec<T> {
+    pub fn one(first: T) -> NonEmptyVec<T> {
+        NonEmptyVec { inner: vec![first] }
+    }
+
+    pub fn with_capacity(first: T, capacity: usize) -> NonEmptyVec<T> {
+        let mut inner = Vec::with_capacity(capacity);
+        inner.push(first);
+        NonEmptyVec { inner }
+    }
+
+    pub fn first(&self) -> &T {
+        &self.inner[0]
+    }
+
+    pub fn tail(&self) -> &[T] {
+        &self.inner[1..]
+    }
+
+    pub fn last(&self) -> &T {
+        &self.inner[self.len() - 1]
+    }
+
+    pub fn init(&self) -> &[T] {
+        &self.inner[..self.len() - 1]
+    }
+
+    pub fn push(&mut self, value: T) {
+        self.inner.push(value)
+    }
+
+    pub fn reverse(&mut self) {
+        self.inner.reverse()
+    }
+
+    pub fn split_first(&self) -> (&T, &[T]) {
+        (self.first(), self.tail())
+    }
+
+    pub fn split_last(&self) -> (&[T], &T) {
+        (self.init(), self.last())
+    }
+
+    pub fn as_non_empty_slice(&self) -> &NonEmptySlice<T> {
+        unsafe { NonEmptySlice::new_unchecked(&self.inner) }
+    }
+
+    pub fn as_slice(&self) -> &[T] {
+        &self.inner
+    }
+
+    pub fn as_vec(&self) -> &Vec<T> {
+        &self.inner
+    }
+
+    pub fn into_vec(self) -> Vec<T> {
+        self.inner
+    }
+
+    pub fn into_boxed_slice(self) -> Box<NonEmptySlice<T>> {
+        let b = self.inner.into_boxed_slice();
+        unsafe { NonEmptySlice::unchecked_boxed(b) }
+    }
+}
+
+impl<T: Clone> NonEmptyVec<T> {
+    pub fn from_init_last(init: &[T], last: T) -> NonEmptyVec<T> {
+        let mut inner = Vec::with_capacity(init.len() + 1);
+        inner.extend_from_slice(init);
+        inner.push(last);
+        NonEmptyVec { inner }
+    }
+
+    pub fn from_first_tail(first: T, tail: &[T]) -> NonEmptyVec<T> {
+        let mut inner = Vec::with_capacity(tail.len() + 1);
+        inner.push(first);
+        inner.extend_from_slice(tail);
+        NonEmptyVec { inner }
+    }
+
+    pub fn extend_from_slice(&mut self, other: &[T]) {
+        self.inner.extend_from_slice(other)
+    }
+}
+
+impl<'a, T> Extend<&'a T> for NonEmptyVec<T>
+where
+    T: 'a + Copy,
+{
+    fn extend<I: IntoIterator<Item = &'a T>>(&mut self, iter: I) {
+        self.inner.extend(iter);
+    }
+}
+
+impl<T> Extend<T> for NonEmptyVec<T> {
+    fn extend<I: IntoIterator<Item = T>>(&mut self, iter: I) {
+        self.inner.extend(iter);
+    }
+}
+
+impl<T> TryFrom<Vec<T>> for NonEmptyVec<T> {
+    type Error = error::Empty;
+
+    fn try_from(vec: Vec<T>) -> Result<Self, Self::Error> {
+        if vec.is_empty() {
+            Err(error::Empty)
+        } else {
+            Ok(NonEmptyVec { inner: vec })
+        }
+    }
+}
+
+impl<T: fmt::Debug> fmt::Debug for NonEmptyVec<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(&*self.inner, f)
+    }
+}
+
+impl<T> IntoIterator for NonEmptyVec<T> {
+    type Item = T;
+    type IntoIter = std::vec::IntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.inner.into_iter()
+    }
+}
+
+impl<'a, T> IntoIterator for &'a NonEmptyVec<T> {
+    type Item = &'a T;
+    type IntoIter = std::slice::Iter<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+impl<T> Deref for NonEmptyVec<T> {
+    type Target = NonEmptySlice<T>;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        self.as_non_empty_slice()
+    }
+}
+
+#[macro_export]
+macro_rules! non_empty_vec {
+   ($($x:expr),+ $(,)?) => {{
+        $crate::NonEmptyVec::try_from(vec![$($x),+]).unwrap()
+   }};
+    ($h:expr) => {
+        $crate::NonEmptyVec::one($h)
+    };
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn one() {
+        let non_empty_vec = NonEmptyVec::one(10);
+        assert_eq!(non_empty_vec.len(), 1);
+        assert_eq!(non_empty_vec.first(), &10);
+        assert_eq!(non_empty_vec.last(), &10);
+        assert!(non_empty_vec.init().is_empty());
+        assert!(non_empty_vec.tail().is_empty());
+    }
+
+    #[test]
+    fn push() {
+        let mut non_empty_vec = NonEmptyVec::one(10);
+
+        non_empty_vec.push(20);
+
+        assert_eq!(non_empty_vec.len(), 2);
+        assert_eq!(non_empty_vec.first(), &10);
+        assert_eq!(non_empty_vec.last(), &20);
+        assert_eq!(non_empty_vec.init(), &[10]);
+        assert_eq!(non_empty_vec.tail(), &[20]);
+
+        non_empty_vec.push(30);
+
+        assert_eq!(non_empty_vec.len(), 3);
+        assert_eq!(non_empty_vec.first(), &10);
+        assert_eq!(non_empty_vec.last(), &30);
+        assert_eq!(non_empty_vec.init(), &[10, 20]);
+        assert_eq!(non_empty_vec.tail(), &[20, 30]);
+    }
+
+    #[test]
+    fn non_empty_vec_macro() {
+        let one = non_empty_vec![10];
+
+        assert_eq!(one.len(), 1);
+        assert_eq!(one.first(), &10);
+        assert_eq!(one.last(), &10);
+        assert!(one.init().is_empty());
+        assert!(one.tail().is_empty());
+
+        let multiple = non_empty_vec![10, 20, 30, 40, 50];
+
+        assert_eq!(multiple.len(), 5);
+        assert_eq!(multiple.first(), &10);
+        assert_eq!(multiple.last(), &50);
+        assert_eq!(multiple.init(), &[10, 20, 30, 40]);
+        assert_eq!(multiple.tail(), &[20, 30, 40, 50]);
+    }
+
+    #[test]
+    fn debug() {
+        let multiple = non_empty_vec![10, 20, 30, 40, 50];
+
+        let result = format!("{multiple:?}");
+        assert_eq!(result, "[10, 20, 30, 40, 50]");
+    }
+
+    #[test]
+    fn reverse() {
+        let mut multiple = non_empty_vec![10, 20, 30, 40, 50];
+        let reverse = non_empty_vec![50, 40, 30, 20, 10];
+
+        multiple.reverse();
+
+        assert_eq!(multiple, reverse);
+    }
+
+    #[test]
+    fn split() {
+        let multiple = non_empty_vec![10, 20, 30, 40, 50];
+
+        assert_eq!(multiple.split_first(), (&10, &[20, 30, 40, 50][..]));
+        assert_eq!(multiple.split_last(), (&[10, 20, 30, 40][..], &50));
+    }
+
+    #[test]
+    fn extend_from_slice() {
+        let mut one = non_empty_vec![10];
+        let multiple = non_empty_vec![10, 20, 30, 40, 50];
+        one.extend_from_slice(&[20, 30, 40, 50]);
+
+        assert_eq!(one, multiple);
+    }
+
+    #[test]
+    fn extend() {
+        let mut one = non_empty_vec![10];
+        let multiple = non_empty_vec![10, 20, 30, 40, 50];
+        one.extend(multiple.iter());
+
+        assert_eq!(one, non_empty_vec![10, 10, 20, 30, 40, 50]);
+    }
+
+    #[test]
+    fn non_empty_vec_of_simple_struct() {
+        // No clone, no PartialEq, no Eq
+        struct Test(usize);
+
+        let non_empty_vec = non_empty_vec![Test(0)];
+
+        assert!(non_empty_vec.first().0 == 0);
+    }
+}
